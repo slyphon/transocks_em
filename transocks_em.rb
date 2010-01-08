4f5045fac4690eedf5cabf47fe83d3bb66e8a6d2
coderrr <coderrr.contact@gmail.com>
2010-01-09 02:13:34 +0700

    osx compat

--- transocks_em.rb
+++ transocks_em.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require 'socket'
 require 'rubygems'
 require 'eventmachine'
@@ -82,14 +84,28 @@ end
 
 class TransocksTCPServer < EM::Connection
   attr_accessor :closed
+
+  def initialize(ipfw_natd_style = false)
+    @ipfw_natd_style = ipfw_natd_style
+  end
+
   def post_init
+    return  if @ipfw_natd_style
+
     orig_host, orig_port = orig_sockaddr
     orig_host = [orig_host].pack("N").unpack("CCCC")*'.'
 
-    puts "connecting to #{orig_host}:#{orig_port}"
+    proxy_to orig_host, orig_port
+  end
 
-    @proxied = EM.connect($connect_host, $connect_port, TransocksClient, self, orig_host, orig_port)
-    proxy_incoming_to @proxied  unless @proxied.closed
+  def receive_data(data)
+    @buf ||= ''
+    @buf << data
+    if @buf.gsub!(/\[DEST (\d+\.\d+\.\d+\.\d+) (\d+)\] *\n/m, '')
+      orig_host, orig_port = $1, $2.to_i
+      proxy_to orig_host, orig_port
+      @proxied.send_data @buf
+    end
   end
 
   def proxy_target_unbound
@@ -98,14 +114,39 @@ class TransocksTCPServer < EM::Connection
 
   def unbind
     self.closed = true
-    @proxied.close_connection_after_writing
+    @proxied.close_connection_after_writing  if @proxied
   end
+
+  private
+
+  def proxy_to(orig_host, orig_port)
+    puts "connecting to #{orig_host}:#{orig_port}"
+
+    @proxied = EM.connect($connect_host, $connect_port, TransocksClient, self, orig_host, orig_port)
+    proxy_incoming_to @proxied  unless @proxied.closed
+  end
+end
+
+if ARGV.size < 3
+  puts <<-EOF
+    Usage: transsocks_em.rb <proxy_to_host> <proxy_to_port> <listen_port> [natd]
+    natd - puts daemon in ipfw/natd bsd mode rather than linux iptables SO_ORIGINAL_DST mode,
+           aka: get the original dest addr/port from the TCP stream rather than from getsockopt.
+           current issue with natd mode is that the original addr wont be sent until the connection
+           sends some initial data (this makes it incompatible with certain protocols, SSH for example)
+  EOF
+  exit 1
 end
 
 $connect_host, $connect_port, $listen_port = ARGV[0,3]
 
+if (ARGV[3] == 'natd')
+  natd = true
+  puts "bsd ipfw/natd mode!"
+end
+
 EM.run do
   EM.error_handler { puts $!, $@ }
 
-  EM.start_server '127.1', $listen_port, TransocksTCPServer
+  EM.start_server '127.0.0.1', $listen_port, TransocksTCPServer, natd
 end
